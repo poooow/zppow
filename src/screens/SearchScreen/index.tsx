d639424df29ab3be31256927712aeac9d888ead3
Petr Kratochvíla <poooow@gmail.com>
2024-09-03 10:32:50 +0200

    Fix linter formating

--- src/screens/SearchScreen/index.tsx
+++ src/screens/SearchScreen/index.tsx
@@ -1,13 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import { FlatList, Text, View, StyleSheet, TextInput, TouchableOpacity, Appearance } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  FlatList,
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Appearance,
+} from 'react-native';
 import styles from '../../styles/styles.js';
 import HTMLView from 'react-native-htmlview';
-import { MenuProvider, Menu, MenuOptions, MenuOption, MenuTrigger } from 'react-native-popup-menu';
-import DownloadIcon from "../../../assets/images/icon_download"
-import ArrowUpIcon from "../../../assets/images/icon_arrow_up"
-import GiraffeIcon from "../../../assets/images/giraffe"
+import {
+  MenuProvider,
+  Menu,
+  MenuOptions,
+  MenuOption,
+  MenuTrigger,
+} from 'react-native-popup-menu';
+import DownloadIcon from '../../../assets/images/icon_download';
+import ArrowUpIcon from '../../../assets/images/icon_arrow_up';
+import GiraffeIcon from '../../../assets/images/giraffe';
 import KeepAwake from 'react-native-keep-awake';
-import { existsDb, getSongList, populateDb } from '../../helpers/db';
+import {existsDb, getSongList, populateDb} from '../../helpers/db';
 
 type ItemProps = {
   item: {
@@ -16,83 +30,129 @@ type ItemProps = {
     title: string;
     text: string;
     snippet: string;
-  }
+  };
 };
 
-export default function SearchScreen(props: { navigation: { navigate: (arg0: string, arg1: { groupname: any; title: any; text: any; darkMode: boolean; }) => void; }; }) {
+export default function SearchScreen(props: {
+  navigation: {
+    navigate: (
+      arg0: string,
+      arg1: {groupname: any; title: any; text: any; darkMode: boolean},
+    ) => void;
+  };
+}) {
   const [state, setState] = useState({
     dbExists: false,
     songList: [],
     appState: '',
     darkMode: Appearance.getColorScheme() === 'dark',
-    keepAwake: false
-  })
+    keepAwake: false,
+  });
 
   useEffect(() => {
     (async () => {
-      const dbExists = await existsDb()
+      const dbExists = await existsDb();
       if (dbExists) {
-        setState({ ...state, songList: await getSongList(""), dbExists: true, appState: "" })
+        setState({
+          ...state,
+          songList: await getSongList(''),
+          dbExists: true,
+          appState: '',
+        });
       }
-    })()
-  }, [])
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const createDb = async () => {
     try {
-      await populateDb((status: string) => { setState({ ...state, appState: status }) })
+      await populateDb((status: string) => {
+        setState({...state, appState: status});
+      });
     } catch (error) {
-      setState({ ...state, appState: error as string })
+      setState({...state, appState: error as string});
     }
-    setState({ ...state, dbExists: true })
-  }
+    setState({...state, dbExists: true});
+  };
 
   const updateSongList = async (query: string) => {
-    if (state.dbExists) setState({ ...state, songList: await getSongList(query) })
-  }
+    if (state.dbExists) {
+      setState({...state, songList: await getSongList(query)});
+    }
+  };
 
   const toggleDarkMode = () => {
-    setState({ ...state, darkMode: !state.darkMode });
+    setState({...state, darkMode: !state.darkMode});
   };
 
   const toggleAwake = () => {
-    if (state.keepAwake) KeepAwake.deactivate();
-    else KeepAwake.activate();
+    if (state.keepAwake) {
+      KeepAwake.deactivate();
+    } else {
+      KeepAwake.activate();
+    }
 
-    setState({ ...state, keepAwake: !state.keepAwake })
-  }
+    setState({...state, keepAwake: !state.keepAwake});
+  };
 
   const renderPopulateDbButton = () => {
     return (
       <TouchableOpacity onPress={() => createDb()}>
-        <DownloadIcon style={{ alignSelf: 'center', marginTop: 10 }} width="150" height="150" />
-        <Text style={[styles.loadDb, { color: state.darkMode ? '#fff' : '#000' }]}>Nahrát zpěvník{"\n"} z internetu (13 MB)</Text>
+        <DownloadIcon
+          style={{alignSelf: 'center', marginTop: 10}}
+          width="150"
+          height="150"
+        />
+        <Text
+          style={[styles.loadDb, {color: state.darkMode ? '#fff' : '#000'}]}>
+          Nahrát zpěvník{'\n'} z internetu (13 MB)
+        </Text>
       </TouchableOpacity>
     );
-  }
+  };
 
-  const renderItem = ({ item }: ItemProps) => {
+  const renderItem = ({item}: ItemProps) => {
     return (
-      <TouchableOpacity onPress={() =>
-        props.navigation.navigate('Song', {
-          groupname: item.groupname,
-          title: item.title,
-          text: item.text,
-          darkMode: state.darkMode,
-        })}>
-        <View style={[styles.item, { backgroundColor: state.darkMode ? '#000' : '#fff' }]}>
-          <Text style={[styles.title, { color: state.darkMode ? '#fff' : '#000' }]}>{item.title}</Text>
-          <Text style={[styles.subtitle, { color: state.darkMode ? '#fff' : '#000' }]}>{item.groupname}</Text>
+      <TouchableOpacity
+        onPress={() =>
+          props.navigation.navigate('Song', {
+            groupname: item.groupname,
+            title: item.title,
+            text: item.text,
+            darkMode: state.darkMode,
+          })
+        }>
+        <View
+          style={[
+            styles.item,
+            {backgroundColor: state.darkMode ? '#000' : '#fff'},
+          ]}>
+          <Text
+            style={[styles.title, {color: state.darkMode ? '#fff' : '#000'}]}>
+            {item.title}
+          </Text>
+          <Text
+            style={[
+              styles.subtitle,
+              {color: state.darkMode ? '#fff' : '#000'},
+            ]}>
+            {item.groupname}
+          </Text>
           <HTMLView
-            value={'<div>' + item.snippet.replace(/\n/g, "") + '</div>'}
+            value={'<div>' + item.snippet.replace(/\n/g, '') + '</div>'}
             stylesheet={stylesHTML}
           />
         </View>
       </TouchableOpacity>
     );
-  }
+  };
 
   return (
-    <View style={[styles.container, { backgroundColor: state.darkMode ? '#000' : '#fff' }]}>
+    <View
+      style={[
+        styles.container,
+        {backgroundColor: state.darkMode ? '#000' : '#fff'},
+      ]}>
       <MenuProvider>
         <View style={styles.searchHeader}>
           <TextInput
@@ -104,44 +164,63 @@ export default function SearchScreen(props: { navigation: { navigate: (arg0: str
           <Menu>
             <MenuTrigger>
               <Text
-                style={[styles.menuTrigger, { color: state.darkMode ? '#666' : '#ccc' }]}>&#x22EE;</Text>
+                style={[
+                  styles.menuTrigger,
+                  {color: state.darkMode ? '#666' : '#ccc'},
+                ]}>
+                &#x22EE;
+              </Text>
             </MenuTrigger>
             <MenuOptions>
               <MenuOption onSelect={() => toggleDarkMode()}>
-                <Text style={styles.menuItem}>{state.darkMode ? 'Zrušit ' : ''}Noční režim</Text>
+                <Text style={styles.menuItem}>
+                  {state.darkMode ? 'Zrušit ' : ''}Noční režim
+                </Text>
               </MenuOption>
               <MenuOption onSelect={() => toggleAwake()}>
-                <Text style={styles.menuItem}>{state.keepAwake ? 'Zhasínat ' : 'Nezhasínat '}displej</Text>
+                <Text style={styles.menuItem}>
+                  {state.keepAwake ? 'Zhasínat ' : 'Nezhasínat '}displej
+                </Text>
               </MenuOption>
             </MenuOptions>
           </Menu>
         </View>
-        {state.appState !== "" || !state.dbExists ?
+        {state.appState !== '' || !state.dbExists ? (
           <View style={styles.stateContainer}>
-            {state.appState ? <Text style={[styles.appState, { color: state.darkMode ? '#666' : '#ccc' }]}>{state.appState}</Text> : null}
+            {state.appState ? (
+              <Text
+                style={[
+                  styles.appState,
+                  {color: state.darkMode ? '#666' : '#ccc'},
+                ]}>
+                {state.appState}
+              </Text>
+            ) : null}
             {state.dbExists ? null : renderPopulateDbButton()}
           </View>
-          : null}
-        {state.songList.length ?
+        ) : null}
+        {state.songList.length ? (
           <View>
             <FlatList
               keyboardShouldPersistTaps={'handled'}
-              keyExtractor={(item) => item.id.toString()}
+              keyExtractor={item => item.id.toString()}
               data={state.songList}
               renderItem={renderItem}
             />
           </View>
-          : null}
-        {state.appState === '' && state.dbExists && !state.songList.length ?
+        ) : null}
+        {state.appState === '' && state.dbExists && !state.songList.length ? (
           <>
             <View style={styles.stateContainer}>
               <ArrowUpIcon width="100" height="100" />
-              <Text></Text>
+              <Text />
               <Text style={styles.appState}>Můžete začít hledat</Text>
             </View>
-            <View style={styles.imageGiraffe}><GiraffeIcon width="250" height="250" /></View>
+            <View style={styles.imageGiraffe}>
+              <GiraffeIcon width="250" height="250" />
+            </View>
           </>
-          : null}
+        ) : null}
       </MenuProvider>
     </View>
   );
@@ -149,7 +228,7 @@ export default function SearchScreen(props: { navigation: { navigate: (arg0: str
 
 const stylesHTML = StyleSheet.create({
   div: {
-    color: '#888888'
+    color: '#888888',
   },
   b: {
     backgroundColor: '#fffb1f44',
--- src/screens/SongScreen/index.tsx
+++ src/screens/SongScreen/index.tsx
@@ -1,52 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Text, View} from 'react-native';
 import styles from '../../styles/styles.js';
-import { TouchableOpacity } from 'react-native';
-import { WebView } from 'react-native-webview';
-import BtnArrowBackWhite from '../../../assets/images/btn_arrow_back_white.js'
-import BtnArrowBack from '../../../assets/images/btn_arrow_back.js'
-import BtnArrowDownWhite from '../../../assets/images/btn_arrow_down_white.js'
-import BtnArrowDown from '../../../assets/images/btn_arrow_down.js'
-import BtnArrowUpWhite from '../../../assets/images/btn_arrow_up_white.js'
-import BtnArrowUp from '../../../assets/images/btn_arrow_up.js'
-import BtnNoteWhite from '../../../assets/images/btn_note_white.js'
-import BtnNote from '../../../assets/images/btn_note.js'
+import {TouchableOpacity} from 'react-native';
+import {WebView} from 'react-native-webview';
+import BtnArrowBackWhite from '../../../assets/images/btn_arrow_back_white.js';
+import BtnArrowBack from '../../../assets/images/btn_arrow_back.js';
+import BtnArrowDownWhite from '../../../assets/images/btn_arrow_down_white.js';
+import BtnArrowDown from '../../../assets/images/btn_arrow_down.js';
+import BtnArrowUpWhite from '../../../assets/images/btn_arrow_up_white.js';
+import BtnArrowUp from '../../../assets/images/btn_arrow_up.js';
+import BtnNoteWhite from '../../../assets/images/btn_note_white.js';
+import BtnNote from '../../../assets/images/btn_note.js';
 import transpose from '../../helpers/transpose';
 
 export default function SongScreen(props: any) {
-
   const [state, setState] = useState({
     transposition: 0,
     darkMode: props.route.params.darkMode,
-    song: { groupname: props.route.params.groupname, title: props.route.params.title, text: props.route.params.text, html: '' }
-  })
+    song: {
+      groupname: props.route.params.groupname,
+      title: props.route.params.title,
+      text: props.route.params.text,
+      html: '',
+    },
+  });
 
   useEffect(() => {
     makeHTML();
-  }, [state.transposition])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state.transposition]);
 
   const transposeUp = () => {
-    setState({ ...state, transposition: state.transposition + 1 });
+    setState({...state, transposition: state.transposition + 1});
   };
 
   const transposeDown = () => {
-    setState({ ...state, transposition: state.transposition - 1 });
+    setState({...state, transposition: state.transposition - 1});
   };
 
   const transposeReset = () => {
-    setState({ ...state, transposition: 0 });
+    setState({...state, transposition: 0});
   };
 
-  /** 
- * Returns transposed chord with html tag
- * @param chord 
- * @returns {string}
- */
+  /**
+   * Returns transposed chord with html tag
+   * @param chord
+   * @returns {string}
+   */
   const transposeHTML = (_: any, chord: string) => {
-    return '<span class="chord">' + transpose(chord, state.transposition) + '</span>';
-  }
+    return (
+      <span className="chord">{transpose(chord, state.transposition)}</span>
+    );
+  };
 
-  /** 
+  /**
    * Transform song text to html for WebView
    */
   const makeHTML = () => {
@@ -116,69 +123,113 @@ export default function SongScreen(props: any) {
         }
         </style>`;
 
-    const bodyHTML = state.song.text.replace(/(.+)/g, '<div class="row">$1&nbsp;</div>')
-      .replace(/\[([a-zA-Z0-9_#+-/()]{2}\])(.{1,2})(\[[a-zA-Z0-9_#+-/()]+)\]/g, '[$1$3]$2')
-      .replace(/\[([a-zA-Z0-9_#+-/()]{3}\])(.{1,3})(\[[a-zA-Z0-9_#+-/()]+)\]/g, '[$1$3]$2')
-      .replace(/\[([a-zA-Z0-9_#+-/()]{4}\])(.{1,4})(\[[a-zA-Z0-9_#+-/()]+)\]/g, '[$1$3]$2')
-      .replace(/\[([a-zA-Z0-9_#+-/()]{5}\])(.{1,5})(\[[a-zA-Z0-9_#+-/()]+)\]/g, '[$1$3]$2')
+    const bodyHTML = state.song.text
+      .replace(/(.+)/g, '<div class="row">$1&nbsp;</div>')
+      .replace(
+        /\[([a-zA-Z0-9_#+-/()]{2}\])(.{1,2})(\[[a-zA-Z0-9_#+-/()]+)\]/g,
+        '[$1$3]$2',
+      )
+      .replace(
+        /\[([a-zA-Z0-9_#+-/()]{3}\])(.{1,3})(\[[a-zA-Z0-9_#+-/()]+)\]/g,
+        '[$1$3]$2',
+      )
+      .replace(
+        /\[([a-zA-Z0-9_#+-/()]{4}\])(.{1,4})(\[[a-zA-Z0-9_#+-/()]+)\]/g,
+        '[$1$3]$2',
+      )
+      .replace(
+        /\[([a-zA-Z0-9_#+-/()]{5}\])(.{1,5})(\[[a-zA-Z0-9_#+-/()]+)\]/g,
+        '[$1$3]$2',
+      )
       .replace(/\[([a-zA-Z0-9_#+-/()\s]+)\]/g, transposeHTML)
-      .replace(/<\/span>\s*<span class="chord">/g, " ")
+      .replace(/<\/span>\s*<span class="chord">/g, ' ')
       .replace(/(?:\r\n|\r|\n)/g, '\n')
-      .replace(/(\.|=)(R[0-9]{0,2}|Ref|Rf|\*|[0-9]{1,2})(\.:|\.|:)/g, '<span class="verseNumber">$2</span>');
+      .replace(
+        /(\.|=)(R[0-9]{0,2}|Ref|Rf|\*|[0-9]{1,2})(\.:|\.|:)/g,
+        '<span class="verseNumber">$2</span>',
+      );
 
-    const resultHTML = '<meta name="viewport" content="width=device-width, initial-scale=1">'
-      + styleHTML + (state.darkMode ? styleHTMLDark : '') + '<div class="container">' + bodyHTML + '</div>';
+    const resultHTML =
+      '<meta name="viewport" content="width=device-width, initial-scale=1">' +
+      styleHTML +
+      (state.darkMode ? styleHTMLDark : '') +
+      '<div class="container">' +
+      bodyHTML +
+      '</div>';
 
-    setState({ ...state, song: { ...state.song, html: resultHTML }});
-  }
+    setState({...state, song: {...state.song, html: resultHTML}});
+  };
 
   return (
-    <View style={[styles.container, { backgroundColor: state.darkMode ? '#000' : '#fff' }]}>
+    <View
+      style={[
+        styles.container,
+        {backgroundColor: state.darkMode ? '#000' : '#fff'},
+      ]}>
       <View style={styles.songHeader}>
-        <TouchableOpacity style={styles.icon} onPress={() => props.navigation.goBack()}>
-          {state.darkMode ?
+        <TouchableOpacity
+          style={styles.icon}
+          onPress={() => props.navigation.goBack()}>
+          {state.darkMode ? (
             <BtnArrowBackWhite width="40" height="40" />
-            :
+          ) : (
             <BtnArrowBack width="40" height="40" />
-          }
+          )}
         </TouchableOpacity>
         <View style={styles.songHeaderTitle}>
-          <Text style={[styles.title, { color: state.darkMode ? '#fff' : '#000' }]}>{state.song.title}</Text>
-          <Text style={[styles.subtitle, { color: state.darkMode ? '#fff' : '#000' }]}>{state.song.groupname}</Text>
+          <Text
+            style={[styles.title, {color: state.darkMode ? '#fff' : '#000'}]}>
+            {state.song.title}
+          </Text>
+          <Text
+            style={[
+              styles.subtitle,
+              {color: state.darkMode ? '#fff' : '#000'},
+            ]}>
+            {state.song.groupname}
+          </Text>
         </View>
         <View style={styles.songHeaderTransposition}>
           <TouchableOpacity onPress={() => transposeDown()}>
-            {state.darkMode ?
+            {state.darkMode ? (
               <BtnArrowDownWhite width="30" height="30" />
-              :
+            ) : (
               <BtnArrowDown width="30" height="30" />
-            }
+            )}
           </TouchableOpacity>
           <TouchableOpacity onPress={() => transposeReset()}>
-            {state.darkMode ?
+            {state.darkMode ? (
               <BtnNoteWhite width="16" height="30" />
-              :
+            ) : (
               <BtnNote width="16" height="30" />
-            }
+            )}
           </TouchableOpacity>
-          <View><Text style={[styles.shift, { color: state.darkMode ? '#fff' : '#000' }]}>{state.transposition ? (state.transposition < 0 ? state.transposition : "+" + state.transposition) : null}</Text></View>
+          <View>
+            <Text
+              style={[styles.shift, {color: state.darkMode ? '#fff' : '#000'}]}>
+              {state.transposition
+                ? state.transposition < 0
+                  ? state.transposition
+                  : '+' + state.transposition
+                : null}
+            </Text>
+          </View>
           <TouchableOpacity onPress={() => transposeUp()}>
-            {state.darkMode ?
+            {state.darkMode ? (
               <BtnArrowUpWhite width="30" height="30" />
-              :
+            ) : (
               <BtnArrowUp width="30" height="30" />
-            }
+            )}
           </TouchableOpacity>
         </View>
       </View>
       <WebView
         originWhitelist={['*']}
-        source={{ html: state.song.html }}
-        style={{ width: '100%', resizeMode: 'cover', flex: 1 }}
+        source={{html: state.song.html}}
+        style={{width: '100%', resizeMode: 'cover', flex: 1}}
       />
     </View>
   );
-
 }
 
 SongScreen.navigationOptions = {
