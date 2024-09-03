e2638fdeb08dd18fd8dc920a79f1d616c2ce33bd
Petr Kratochvíla <poooow@gmail.com>
2024-09-03 10:35:13 +0200

    Fix linter formating

--- src/helpers/db.ts
+++ src/helpers/db.ts
@@ -1,47 +1,50 @@
-import { enablePromise, openDatabase, SQLiteDatabase } from 'react-native-sqlite-storage';
+import {enablePromise, openDatabase} from 'react-native-sqlite-storage';
 const parseString = require('react-native-xml2js').parseString;
 const XML_URL = 'https://raw.githubusercontent.com/poooow/zppow/main/inet.xml';
 enablePromise(true);
 
 export const getDBConnection = async () => {
-  return openDatabase({ name: 'DbZpevnikator4', location: 'default' });
+  return openDatabase({name: 'DbZpevnikator4', location: 'default'});
 };
 
 export async function existsDb(): Promise<boolean> {
   const db = await getDBConnection();
   try {
-    const results = await db.executeSql(`SELECT name FROM sqlite_master WHERE type='table' AND name='zpevnikator';`)
-    return results[0].rows.length ? true : false
+    const results = await db.executeSql(
+      `SELECT name FROM sqlite_master WHERE type='table' AND name='zpevnikator';`,
+    );
+    return results[0].rows.length ? true : false;
   } catch (error) {
     console.error(error);
-    return false
+    return false;
   }
-};
+}
 
 export async function purgeDb() {
-  if (await existsDb() === false) return false;
+  if ((await existsDb()) === false) return false;
 
   const db = await getDBConnection();
   try {
-    const results = await db.executeSql('DROP TABLE zpevnikator;')
+    await db.executeSql('DROP TABLE zpevnikator;');
   } catch (error) {
     console.error(error);
-    return false
+    return false;
   }
 }
 
 async function createDb() {
   const db = await getDBConnection();
   try {
-    const results = await db.executeSql('CREATE VIRTUAL TABLE IF NOT EXISTS zpevnikator USING fts4(id INTEGER PRIMARY KEY AUTOINCREMENT, groupname TEXT, title TEXT, text TEXT, textclean TEXT, tokenize=unicode61);')
+    await db.executeSql(
+      'CREATE VIRTUAL TABLE IF NOT EXISTS zpevnikator USING fts4(id INTEGER PRIMARY KEY AUTOINCREMENT, groupname TEXT, title TEXT, text TEXT, textclean TEXT, tokenize=unicode61);',
+    );
   } catch (error) {
     console.error(error);
-    return false
+    return false;
   }
 }
 
 export async function populateDb(setStatus: (status: string) => void) {
-
   createDb();
 
   setStatus('Nahrávám zpěvník ...');
@@ -53,16 +56,16 @@ export async function populateDb(setStatus: (status: string) => void) {
     xmlData = await xmlData.text();
   } catch (error) {
     console.error(error);
-    return
+    return;
   }
 
   let jsonData: any;
-  parseString(xmlData, (_err: any, result: any) => jsonData = result);
+  parseString(xmlData, (_err: any, result: any) => (jsonData = result));
 
   if (jsonData) {
     setStatus('Zpracovávám zpěvník ...');
-  }  else {
-    purgeDb()
+  } else {
+    purgeDb();
     setStatus('Nepodařilo se stáhnout zpěvník');
     return;
   }
@@ -72,20 +75,26 @@ export async function populateDb(setStatus: (status: string) => void) {
   const db = await getDBConnection();
 
   try {
-    db.transaction((tx) => {
+    db.transaction(tx => {
       for (let i = 0; i < song.length; i++) {
-        let groupname = song[i].groupname.toString().replace(/\"/g, '&quot;').replace(/\"/g, '&quot;');
+        let groupname = song[i].groupname
+          .toString()
+          .replace(/\"/g, '&quot;')
+          .replace(/\"/g, '&quot;');
         let title = song[i].title.toString().replace(/\"/g, '&quot;');
         let text = song[i].songtext.toString().replace(/\"/g, '&quot;');
         let textclean = text.replace(/\[(.{1,7}?)\]/g, '');
 
-        tx.executeSql('INSERT INTO zpevnikator (groupname, title, text, textclean) VALUES (?,?,?,?)', [groupname, title, text, textclean])
+        tx.executeSql(
+          'INSERT INTO zpevnikator (groupname, title, text, textclean) VALUES (?,?,?,?)',
+          [groupname, title, text, textclean],
+        );
       }
-    })
+    });
   } catch (error) {
     setStatus('Nepodařilo se uložit zpěvník');
     console.error(error);
-    return
+    return;
   }
 }
 
@@ -94,16 +103,17 @@ export async function populateDb(setStatus: (status: string) => void) {
  * @param query query string
  */
 export async function getSongList(query: string) {
-
-  query = '*' + query + '*'
+  query = '*' + query + '*';
 
   const db = await getDBConnection();
 
   let songList: any = [];
 
   await db.transaction(async tx => {
-
-    var [tx, results] = await tx.executeSql('SELECT groupname,title,text,snippet(zpevnikator, "<b>", "</b>","...", 4, 10) AS snippet FROM zpevnikator WHERE title MATCH ? OR groupname MATCH ? OR zpevnikator MATCH ? LIMIT 42', [query, query, query])
+    var [tx, results] = await tx.executeSql(
+      'SELECT groupname,title,text,snippet(zpevnikator, "<b>", "</b>","...", 4, 10) AS snippet FROM zpevnikator WHERE title MATCH ? OR groupname MATCH ? OR zpevnikator MATCH ? LIMIT 42',
+      [query, query, query],
+    );
 
     for (let i = 0; i < results.rows.length; ++i) {
       songList.push({
@@ -111,11 +121,10 @@ export async function getSongList(query: string) {
         groupname: results.rows.item(i).groupname,
         title: results.rows.item(i).title,
         text: results.rows.item(i).text,
-        snippet: results.rows.item(i).snippet
-      })
-
+        snippet: results.rows.item(i).snippet,
+      });
     }
   });
 
-  return songList
+  return songList;
 }
